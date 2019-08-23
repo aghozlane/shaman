a00ddcf1771526f6ef10af7c005db5340bdb08ab
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2019-08-23 18:09:46 +0200

    Update for conda

--- www/shaman_conda.R
+++ www/shaman_conda.R
@@ -1,6 +1,6 @@
-#!/usr/bin/Rscript --vanilla
+#!/usr/bin/env Rscript --vanilla
 prefix=Sys.getenv(x = "PREFIX")
 prefix=paste(prefix, "bin", sep=.Platform$file.sep)
 system(paste("python ", prefix, "shaman_bioblend/shaman_bioblend.py -w ", prefix,"shamanapp/www/masque/ -s -d", sep=.Platform$file.sep))
-system(paste("Rscript -e 'library(\"shiny\");runApp(appDir=", prefix,"KronaRShy\", port=5438)'", sep=.Platform$file.sep, wait=FALSE)
+system(paste(prefix, "Rscript -e 'library(\"shiny\");runApp(appDir=", prefix,"KronaRShy\", port=5438)'", sep=.Platform$file.sep, wait=FALSE)
 runApp(paste("appDir= ", prefix, "shamanapp", sep=.Platform$file.sep)
