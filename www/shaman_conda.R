0304161a59e79ba6030624bf994539fca6272458
Amine  GHOZLANE <amine.ghozlane@pasteur.fr>
2019-08-23 11:42:07 +0200

    Update for conda

--- www/shaman_conda.R
+++ www/shaman_conda.R
@@ -2,4 +2,4 @@
 prefix=Sys.getenv(x = "PREFIX")
 prefix=paste(prefix, "bin", sep=.Platform$file.sep)
 system(paste("Rscript -e 'library(\"shiny\");runApp(appDir=", prefix,"KronaRShy\", port=5438)'", sep=.Platform$file.sep, wait=FALSE)
-runApp(paste("appDir= ", prefix, "shaman", sep=.Platform$file.sep)
+runApp(paste("appDir= ", prefix, "shamanapp", sep=.Platform$file.sep)
